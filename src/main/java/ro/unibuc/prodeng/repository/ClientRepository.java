7343697a532cf4d6b214d7ff84f518eff032c87c
andreiOpran <andrei.opran@icloud.com>
2026-03-08 15:41:33 +0200

    [FEAT] create client service
    included atomic  update at the repository level to have safe concurrency

--- src/main/java/ro/unibuc/prodeng/repository/ClientRepository.java
+++ src/main/java/ro/unibuc/prodeng/repository/ClientRepository.java
@@ -1,6 +1,8 @@
 package ro.unibuc.prodeng.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.prodeng.model.ClientEntity;
 
@@ -9,4 +11,12 @@ import java.util.Optional;
 @Repository
 public interface ClientRepository extends MongoRepository<ClientEntity, String> {
     Optional<ClientEntity> findByEmail(String email);
+
+    @Query("{ '_id': ?0 }")
+    @Update("{ '$inc': { 'completedProjects': 1 } }")
+    void incrementCompletedProjects(String id);
+
+    @Query("{ '_id': ?0 }")
+    @Update("{ '$inc': { 'cancelledProjects': 1 } }")
+    void incrementCancelledProjects(String id);
 }
