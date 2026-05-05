f4b004e3382a980db9892735ffe23c1db0038d06
andreiOpran <andrei.opran@icloud.com>
2026-05-05 08:37:17 +0300

    [FEAT] jmeter run headless app scripts

---
+++ src/test/java/ro/unibuc/prodeng/jmeter/run_headless_client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set variables for clarity
+JMX=/home/andrei/Desktop/hirewave/src/test/java/ro/unibuc/prodeng/jmeter/clients_tests.jmx
+JMETER=/home/andrei/Downloads/apache-jmeter-5.6.3/bin/jmeter
+LOGDIR="$(dirname "$JMX")"
+OUTDIR="$LOGDIR/client_logs"
+REPORT_DIR="$OUTDIR/report"
+
+mkdir -p "$OUTDIR"
+rm -rf "$REPORT_DIR"
+
+"$JMETER" -n -t "$JMX" \
+  -l "$OUTDIR/jmeter_results.jtl" \
+  -j "$OUTDIR/jmeter.log" \
+  -e -o "$REPORT_DIR"
